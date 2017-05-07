bbb7b41ba2f45b60bab12890ca17e242399bd129
CLRB17 <cynthia@brown-house.net>
2017-05-07 11:40:34 -0400

    added promptly.  am working through syntax

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var promptly = require("promptly");
 // var db = require("./Bamazon.sql");
 
 
@@ -22,5 +23,29 @@ connection.query("SELECT * FROM products", function(err, resp) {
 		console.log("Name: " + resp[i].product_name);
 		console.log("Department: " + resp[i].department_name);
 		console.log("Price: $" + resp[i].price + "\n");
-	}
+	};
+
+console.log("howdy");
+
+function ID() {
+promptly.prompt('What is the ID (1-10) of the product you would like? ', function (err, value) {
+    // err is always null in this case, because no validators are set 
+//     console.log(value);
+	var productID = value;
+	console.log(productID);
+	buyQuantity();
 });
+};
+
+function buyQuantity() {
+promptly.prompt('How many units of the product they would like to buy?', function (err, value) {
+//     // err is always null in this case, because no validators are set 
+//     console.log(value);
+	var quantity = value;
+	console.log(quantity);
+// });
+});
+};
+
+
+
